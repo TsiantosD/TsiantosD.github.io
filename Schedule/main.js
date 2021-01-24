543d1b9efd5510abdc7b83c3ab9eeddd7f9c06dc
TsiantosD <tsiantos.d@gmail.com>
2021-01-24 22:59:38 +0200

    fixed dark/ light theme bug

--- Schedule/main.js
+++ Schedule/main.js
@@ -1,19 +1,29 @@
+
+if (!localStorage.getItem("theme")) {
+    localStorage.setItem("theme", "dark_theme");
+}   
+
+
 document.addEventListener("DOMContentLoaded", function() {
-    
-    const dark_button = document.querySelector("button");
+    let theme = localStorage.getItem("theme");
+    document.querySelector("body").classList.add(`${theme}`);
     document.querySelector("button").onclick = () => {
-        if (dark_button.innerHTML === "Dark Theme") {
-            dark_button.innerHTML = "Light Theme";
-            document.querySelector("body").classList.remove("dark_theme")
-            document.querySelector("body").classList.add("light_theme")
-            
-        } else {
+        const dark_button = document.querySelector("button");
+        // if the current theme is dark and the "light theme" button is clicked
+        if (dark_button.innerHTML === "Light Theme") { 
             dark_button.innerHTML = "Dark Theme";
-            document.querySelector("body").classList.remove("light_theme")
-            document.querySelector("body").classList.add("dark_theme")
+            document.querySelector("body").classList.remove(`${theme}`);
+            theme = "light_theme"
+            document.querySelector("body").classList.add(`${theme}`);
+            localStorage.setItem("theme", theme)
+            
+            // if the current theme is light and the "dark theme" button is clicked
+        } else {
+            dark_button.innerHTML = "Light Theme";
+            document.querySelector("body").classList.remove(`${theme}`);
+            theme = "dark_theme"
+            document.querySelector("body").classList.add(`${theme}`);
+            localStorage.setItem("theme", theme)
         }
     }
-
-    
-
 });
